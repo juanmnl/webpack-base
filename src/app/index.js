06d914c2f11a5a802736f7491fb6e3ccf442e9de
juanmnl <juanmnlcornejo@gmail.com>
2017-12-15 16:27:22 -0500

    Add layout overlay tool

--- src/app/index.js
+++ src/app/index.js
@@ -6,3 +6,11 @@ let date = new Date();
 
 const root = document.getElementById('root');
 root.append(date);
+
+let toggleLayout = document.getElementById('toggleLayout');
+let screen = document.body;
+
+toggleLayout.onclick = function() {
+  screen.classList.toggle('layout');
+  toggleLayout.classList.toggle('active');
+};
